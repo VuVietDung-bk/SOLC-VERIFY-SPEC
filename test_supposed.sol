2dfdaf499ba2c264f8ef5e8831c641a09f3b258c
root <root@DESKTOP-LSPTBSP>
2025-08-29 10:18:03 +0700

    Add dockerfiles

---
+++ test_supposed.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.7.0;
+
+contract Example1 {
+    uint public x;
+    uint public y;
+
+    /// @notice precondition n >= 0
+    /// @notice postcondition x >= __verifier_old_uint(x)
+    function add_to_x(uint n) external {
+        x = x + n;
+        //require(x >= y); // Ensures that there is no overflow
+        _add(n);
+    }
+
+    /// @notice precondition n >= 0
+    /// @notice postcondition x >= __verifier_old_uint(x)
+    function _add(uint n) internal {
+        x = x + n;
+        y = y + n;
+    }
+    
+    function add_to_y(uint n) external {
+        y = y + n;
+        //require(x >= y); // Ensures that there is no overflow
+    }
+    
+    // function add(int n) public {
+    //     require(n >= 0);
+    //     add_to_x(n);
+    //     /// @notice invariant y <= x
+    //     while (y < x) {
+    //         y = y + 1;
+    //     }
+    // }
+}
